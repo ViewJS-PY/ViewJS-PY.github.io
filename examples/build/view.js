056ea8977ab8ebe3c0b9e2cf600e1858b9f2b3b6
ViewJS-PY <36113078+ViewJS-PY@users.noreply.github.com>
2018-02-03 20:23:23 -0500

    Add files via upload

--- examples/build/view.js
+++ examples/build/view.js
@@ -115,4 +115,5 @@ ViewPort.prototype.updateCustomStats = function (pin, by) {
 	}
 };
 var VIEW = {"ViewPort": ViewPort, "RIGHT":"right", "LEFT":"left", "UP":"up", "DOWN":"down", "REVISION": "View.js v2.0"};
-console.log("%c" + VIEW.REVISION, 'color:#00aaff; background-color:black; font-size:30px; font-family:sans-serif;line-height:1.15;');
+//console.log("%c" + VIEW.REVISION, 'color:#00aaff; background-color:black; font-size:30px; font-family:sans-serif;line-height:1.15;');
+console.log('%c '+VIEW.REVISION, 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
